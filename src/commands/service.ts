68029d4c071e1f4f56120d3b433e896fa770b5db
retgits <lstigter@gmail.com>
2021-02-05 17:28:03 -0800

    feat: add services logs command

--- src/commands/service.ts
+++ src/commands/service.ts
@@ -5,9 +5,9 @@ import { Convert } from '../datatypes/converter';
 export async function deployService(service: string, image: string, projectID: string, { dryrun, silent, configFile, context }: CommandInput): Promise<ShellResult> {
     // Create the command
     const command = new Command('akkasls services deploy');
-    command.addParameter({name: 'service', value: service, addNameToCommand: false});
-    command.addParameter({name: 'image', value: image, addNameToCommand: false});
-    command.addParameter({name: 'project', value: projectID, addNameToCommand: true});
+    command.addParameter({ name: 'service', value: service, addNameToCommand: false });
+    command.addParameter({ name: 'image', value: image, addNameToCommand: false });
+    command.addParameter({ name: 'project', value: projectID, addNameToCommand: true });
 
     // Set parameters
     command.setSilent(silent);
@@ -15,7 +15,7 @@ export async function deployService(service: string, image: string, projectID: s
     command.setContext(context);
 
     // Run the command
-    if(dryrun) {
+    if (dryrun) {
         return command.dryRun();
     }
 
@@ -25,9 +25,9 @@ export async function deployService(service: string, image: string, projectID: s
 export async function exposeService(service: string, flags: string, projectID: string, { dryrun, silent, configFile, context }: CommandInput): Promise<ShellResult> {
     // Create the command
     const command = new Command('akkasls services expose',);
-    command.addParameter({name: 'service', value: service, addNameToCommand: false});
-    command.addParameter({name: 'project', value: projectID, addNameToCommand: true});
-    command.addParameter({name: 'flags', value: flags, addNameToCommand: false});
+    command.addParameter({ name: 'service', value: service, addNameToCommand: false });
+    command.addParameter({ name: 'project', value: projectID, addNameToCommand: true });
+    command.addParameter({ name: 'flags', value: flags, addNameToCommand: false });
 
     // Set parameters
     command.setSilent(silent);
@@ -35,17 +35,17 @@ export async function exposeService(service: string, flags: string, projectID: s
     command.setContext(context);
 
     // Run the command
-    if(dryrun) {
+    if (dryrun) {
         return command.dryRun();
     }
 
     return await command.run();
 }
 
-export async function listServices(projectID: string,{ dryrun, silent, configFile, context }: CommandInput): Promise<ShellResult> {
+export async function listServices(projectID: string, { dryrun, silent, configFile, context }: CommandInput): Promise<ShellResult> {
     // Create the command
     const command = new Command('akkasls services list -o json');
-    command.addParameter({name: 'project', value: projectID, addNameToCommand: true});
+    command.addParameter({ name: 'project', value: projectID, addNameToCommand: true });
 
     // Set parameters
     command.setSilent(silent);
@@ -53,12 +53,12 @@ export async function listServices(projectID: string,{ dryrun, silent, configFil
     command.setContext(context);
 
     // Run the command
-    if(dryrun) {
+    if (dryrun) {
         return command.dryRun();
     }
 
     const result = await command.run();
-    if (result.code === 0 ) {
+    if (result.code === 0) {
         result.response = Convert.toServices(result?.stdout);
     }
     return result;
@@ -67,8 +67,8 @@ export async function listServices(projectID: string,{ dryrun, silent, configFil
 export async function undeployService(service: string, projectID: string, { dryrun, silent, configFile, context }: CommandInput): Promise<ShellResult> {
     // Create the command
     const command = new Command('akkasls services undeploy');
-    command.addParameter({name: 'service', value: service, addNameToCommand: false});
-    command.addParameter({name: 'project', value: projectID, addNameToCommand: true});
+    command.addParameter({ name: 'service', value: service, addNameToCommand: false });
+    command.addParameter({ name: 'project', value: projectID, addNameToCommand: true });
 
     // Set parameters
     command.setSilent(silent);
@@ -76,7 +76,7 @@ export async function undeployService(service: string, projectID: string, { dryr
     command.setContext(context);
 
     // Run the command
-    if(dryrun) {
+    if (dryrun) {
         return command.dryRun();
     }
 
@@ -86,9 +86,9 @@ export async function undeployService(service: string, projectID: string, { dryr
 export async function unexposeService(service: string, hostname: string, projectID: string, { dryrun, silent, configFile, context }: CommandInput): Promise<ShellResult> {
     // Create the command
     const command = new Command('akkasls services unexpose');
-    command.addParameter({name: 'service', value: service, addNameToCommand: false});
-    command.addParameter({name: 'hostname', value: hostname, addNameToCommand: false});
-    command.addParameter({name: 'project', value: projectID, addNameToCommand: true});
+    command.addParameter({ name: 'service', value: service, addNameToCommand: false });
+    command.addParameter({ name: 'hostname', value: hostname, addNameToCommand: false });
+    command.addParameter({ name: 'project', value: projectID, addNameToCommand: true });
 
     // Set parameters
     command.setSilent(silent);
@@ -96,7 +96,26 @@ export async function unexposeService(service: string, hostname: string, project
     command.setContext(context);
 
     // Run the command
-    if(dryrun) {
+    if (dryrun) {
+        return command.dryRun();
+    }
+
+    return await command.run();
+}
+
+export async function getServiceLogs(service: string, projectID: string, { dryrun, silent, configFile, context }: CommandInput): Promise<ShellResult> {
+    // Create the command
+    const command = new Command('akkasls services logs');
+    command.addParameter({ name: 'service', value: service, addNameToCommand: false });
+    command.addParameter({ name: 'project', value: projectID, addNameToCommand: true });
+
+    // Set parameters
+    command.setSilent(silent);
+    command.setConfigFile(configFile);
+    command.setContext(context);
+
+    // Run the command
+    if (dryrun) {
         return command.dryRun();
     }
 
